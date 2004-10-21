c868205701203c4875a3d5d1020292226e81b41d
ptiyo <ptiyo>
2004-10-21 07:12:19 +0000

    Restauration des fichiers perdus suite à des problèmes avec cvs

---
+++ apes2/src/java/org/ipsquad/apes/ApesClipboardManager.java
@@ -0,0 +1,137 @@
+/*
+ * APES is a Process Engineering Software
+ * Copyright (C) 2003-2004 IPSquad
+ * team@ipsquad.tuxfamily.org
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.ipsquad.apes;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.util.ArrayList;
+
+import org.ipsquad.apes.adapters.ApesTransferable;
+import org.ipsquad.apes.ui.GraphFrame;
+import org.jgraph.JGraph;
+
+/**
+ *
+ * @version $Revision: 1.13 $
+ */
+public class ApesClipboardManager
+{
+	private static ArrayList mGraphCellsList ;
+	
+	public static  Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard(); 
+	
+	public static void copy() 
+	{
+		Project p = Context.getInstance().getProject();
+		if( Context.getInstance().getTopLevelFrame().getDesktop().getSelectedFrame() == null )
+		{
+			return;
+		}
+		
+		JGraph graph = ((GraphFrame)Context.getInstance().getTopLevelFrame().getDesktop().getSelectedFrame()).getGraph();
+		Object [] listCells = graph.getSelectionCells() ;
+		ApesTransferable transfer = new ApesTransferable(p, listCells) ;
+		cb.setContents(transfer, null) ;
+	}
+	
+	public static void cut() 
+	{
+		Project p = Context.getInstance().getProject();
+		
+		if( Context.getInstance().getTopLevelFrame().getDesktop().getSelectedFrame() == null )
+		{
+			return;
+		}
+		
+		JGraph graph = ((GraphFrame)Context.getInstance().getTopLevelFrame().getDesktop().getSelectedFrame()).getGraph();
+		Object [] listCells = graph.getSelectionCells() ; 
+		ApesTransferable transfer = new ApesTransferable(p, listCells) ;
+		
+		cb.setContents(transfer, null) ;
+		graph.getGraphLayoutCache().remove(listCells);
+	}
+	
+	public static void paste()
+	{
+		/*if( Context.getInstance().getTopLevelFrame().getDesktop().getSelectedFrame() == null )
+		{
+			return;
+		}
+		
+		JGraph graph = ((GraphFrame)Context.getInstance().getTopLevelFrame().getDesktop().getSelectedFrame()).getGraph();
+		Transferable t = cb.getContents(mGraphCellsList);
+		SpemGraphAdapter adapter = (SpemGraphAdapter)graph.getModel();
+		int projectHashCode;
+		Vector cells = null;
+		
+		// Make sure the clipboard is not empty.
+		if (t == null)
+		{
+			System.out.println("The clipboard is empty.");
+			return;
+		}
+		try 
+		{
+			cells = (Vector)t.getTransferData(ApesTransferable.mArrayFlavor);
+			projectHashCode = ((Integer)cells.remove(0)).intValue();
+			
+			if( Context.getInstance().getProject().hashCode() != projectHashCode )
+			{	
+				for( int i = 0; i < cells.size(); i++ )
+				{
+					if( cells.get(i) instanceof ApesGraphCell )
+					{
+						ApesGraphCell cell = (ApesGraphCell)cells.get(i);
+						cell.setUserObject(((Element)cell.getUserObject()).clone());
+					}
+				}
+			}
+			else
+			{
+				for( int i = 0; i < cells.size(); i++ )
+				{
+					if( cells.get(i) instanceof ApesGraphCell )
+					{
+						ApesGraphCell cell = (ApesGraphCell)cells.get(i);
+						Element element = (Element) cell.getUserObject(); 
+						Object userObject = ApesMediator.getInstance().findByID(element.getID());
+						if( userObject != null )
+						{
+							cell.setUserObject(userObject);
+						}
+					}
+				}
+			}
+			
+
+			adapter.insertCells( cells );
+		} 
+		catch (IOException e) 
+		{
+			System.out.println("Données non disponible");
+		} 
+		catch (UnsupportedFlavorException e) {
+			System.out.println("DataFlower de mauvais type");
+		}*/
+	}
+
+}
---
+++ apes2/src/java/org/ipsquad/apes/MainFrameInterface.java
@@ -0,0 +1,48 @@
+/*
+ * APES is a Process Engineering Software
+ * Copyright (C) 2003-2004 IPSquad
+ * team@ipsquad.tuxfamily.org
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+package org.ipsquad.apes;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JTree;
+
+import org.ipsquad.apes.ui.GraphFrame;
+import org.ipsquad.apes.ui.ToolPalette;
+import org.jgraph.graph.GraphModel;
+
+/**
+ * Interface required to be the main frame of the application
+ *
+ * @version $Revision: 1.4 $
+ */
+public interface MainFrameInterface
+{
+	public void setFilePath(String filePath);
+	public JTree getTree();
+	public GraphFrame getGraphFrame( GraphModel model );
+	public JDesktopPane getDesktop();
+	public void setToolPalette(ToolPalette toolPalette);
+	public ToolPalette getToolPalette();
+	public void openDiagram(GraphModel model);
+	public void deleteDiagram(GraphModel model);
+	public void setDefaultToolPalette();
+}
